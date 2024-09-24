76d10c660aedb9676a347ae1126a0597ca7ae74e
RodrigoStrapazzon <Rodrigo93rs@gmail.com>
2024-09-24 15:39:44 -0300

    componentizando

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/RodrigoStrapazzon.png" />
+    <Titulo>Rodrigo Strapazzon</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sint
+      voluptas delectus deleniti dolores, aperiam recusandae, corrupti
+      blanditiis perspiciatis aspernatur accusamus. Minus veniam nobis
+      voluptatibus nemo corporis aliquam mollitia perspiciatis?
+    </p>
+  </section>
+)
+
+export default Sobre
