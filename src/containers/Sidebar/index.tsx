142fa0e36d6827ba3b0ca9e8491fccde167612ee
RodrigoStrapazzon <Rodrigo93rs@gmail.com>
2024-09-27 11:13:01 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/RodrigoStrapazzon.png" />
-    <Titulo fontSize={20}>Rodrigo Strapazzon</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rodrigo Strapazzon</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        RodrigoStrapazzon
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        {'Desenvolvedor Python Full Stack'}
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
