347cf04d20bd41c8b41b60d657b9f42ef3730818
RodrigoStrapazzon <Rodrigo93rs@gmail.com>
2024-09-27 17:18:31 -0300

    inclusao de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [estaUsandotemadark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandotemadark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandotemadark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
